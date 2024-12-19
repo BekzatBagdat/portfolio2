1e40ba2a3b43ef7d32a28cadff84203823f0cb07
Bekzat Bagdat <paintdex12@gmail.com>
2024-12-19 12:09:08 +0100

    updt video

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,30 @@ import AboutSection from './components/AboutSection';
 import ProjectsSection from './components/ProjectsSection';
 import Footer from './components/Footer';
 import './main.css';
+import video from '../src/video/videoBg.mp4';
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HeroSection />} />
-          <Route path="/about" element={<AboutSection />} />
-          <Route path="/projects" element={<ProjectsSection />} />
-        </Routes>
-        <Footer />
-      </Router>
+    <div className="app-container flex flex-col min-h-screen">
+      {/* Background Video */}
+      <video autoPlay loop muted playsInline className="video-background">
+        <source src={video} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      {/* Content */}
+      <div className="content-overlay flex-grow">
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<HeroSection />} />
+            <Route path="/about" element={<AboutSection />} />
+            <Route path="/projects" element={<ProjectsSection />} />
+          </Routes>
+        </Router>
+      </div>
+
+      {/* Footer */}
+      <Footer />
     </div>
   );
 }
